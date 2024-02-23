6b9f415dac1bf49a3db113b12123d050cc880295
joe <joe@localhost.localdomain>
2024-02-23 11:42:57 -0500

    Add: initial release

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/blacktrack-wallpaper-official/issues
+# Bug-Submit: https://github.com/<user>/blacktrack-wallpaper-official/issues/new
+# Changelog: https://github.com/<user>/blacktrack-wallpaper-official/blob/master/CHANGES
+# Documentation: https://github.com/<user>/blacktrack-wallpaper-official/wiki
+# Repository-Browse: https://github.com/<user>/blacktrack-wallpaper-official
+# Repository: https://github.com/<user>/blacktrack-wallpaper-official.git
